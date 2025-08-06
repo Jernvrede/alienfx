5090d2d692b8990bb6910f577625f27dbd267d78
Jernvrede <knucklehead.pb@gmail.com>
2025-08-06 19:11:31 +0200

    Updating with Desktop R4.

--- alienfx/core/prober.py
+++ alienfx/core/prober.py
@@ -37,6 +37,7 @@ import usb.core
 from alienfx.core.controller import AlienFXController as AlienFXController
 
 """ Import all subclasses of AlienFXController here. """
+import alienfx.core.controller_desktopr4 ## Due to conflicting controllers this is added first (Desktop R4 Version)
 import alienfx.core.controller_a51m
 import alienfx.core.controller_area51
 import alienfx.core.controller_area51_r2
